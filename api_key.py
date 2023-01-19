08511a3f0158c301d28093c3f8a036af6792a720
Max <86234226+MaxSSD@users.noreply.github.com>
2023-01-19 22:45:38 +0530

    Create api_key.py

---
+++ api_key.py
@@ -0,0 +1,7 @@
+import openai
+
+# Get your at --> https://beta.openai.com/account/api-keys
+key = openai.api_key = ""
+# Use your own API ID and HASH, which you can get from https://my.telegram.org/apps
+
+bot_token = ""
