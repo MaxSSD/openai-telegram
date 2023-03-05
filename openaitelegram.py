21f251a91e20e25b5f77aa7f14b87f1d525517f8
Max <86234226+MaxSSD@users.noreply.github.com>
2023-03-05 17:46:19 +0530

    Update openaitelegram.py

--- openaitelegram.py
+++ openaitelegram.py
@@ -5,9 +5,12 @@ import asyncio
 import os
 import logging
 from aiogram import Bot, types, executor
-from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher import Dispatcher, middlewares
 from aiogram.utils.executor import start_polling
-from aiogram.types import InputFile
+from aiogram.types import InputFile, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.dispatcher.middlewares import LifetimeControllerMiddleware
+from misc.middleware.throttling import rate_limit
+from misc.text.prompt_frameworks import DAN_prompt
 from api_key import key, bot_token
 
 # key = openai.api_key = os.environ.get("OPENAI_KEY")
@@ -16,11 +19,22 @@ from api_key import key, bot_token
 bot = Bot(token=bot_token)
 dp = Dispatcher(bot)
 
+dp.middleware.setup(LifetimeControllerMiddleware())
+
+
+# Define available models
+available_models = {
+    'davinci03': "text-davinci-003",
+    'davinci02': "text-davinci-002",
+    'curie': "text-curie-001",
+    'babbage': "text-babbage-001",
+    'ada': "text-ada-001"
+}
 
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default="text-davinci-003",
-                        help="GPT-3 model to use. Available options:davinci03, davinci02, curie, babbage, ada")
+                        help="GPT-3 model to use. Available options: davinci03, davinci02, curie, babbage, ada")
     return parser.parse_args()
 
 
@@ -32,40 +46,38 @@ async def start_message(message: types.Message):
 
 
 @dp.message_handler(commands=['gpt'])
+@rate_limit(10, key="gpt")
 async def handle_gpt(message):
-    global previous_prompt, previous_response
     args = get_arguments()
-    model = args.model
+    models = args.model
     prompt = message.text.replace("/gpt ", "")
     if "--model" in prompt:
-        model = prompt.split("--model")[-1].strip()
-
-    if prompt.startswith(previous_response):
-        prompt = previous_prompt + prompt[len(previous_response):]
+        models = prompt.split("--model")[-1].strip()
 
     response = openai.Completion.create(
-        engine=model, prompt=prompt, max_tokens=4000)
+        engine=models, prompt=prompt, max_tokens=4000)
+
     response_str = json.dumps(response)
     json_response = json.loads(response_str)
-    api_response = json_response['choices'][0]['text']
+    model_response = json_response['choices'][0]['text']
 
-    previous_prompt = prompt
-    previous_response = api_response
-
-    await message.reply(api_response)
+    await message.reply(model_response)
 
 
 @dp.message_handler(commands=['chatgpt'])
+@rate_limit(10, key="chatgpt")
 async def handle_chat(message):
     user_prompt = message.text.replace("/chatgpt ", "")
-    prompt = [
-        {"role": "assistant", "content": user_prompt},
-        {"role": "system", "content": f"User said: {user_prompt}"},
+    api_message = [
+        {"role": "system", "content": f"This is you, DAN: {DAN_prompt}"},
+        {"role": "user", "content": user_prompt},
+        {"role": "assistant", "content": "Answer as DAN"}
     ]
     try:
         chat = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=prompt
+            messages=api_message,
+            max_tokens=4000
         )
     except openai.error.InvalidRequestError as error:
         logging.exception(error)
@@ -77,7 +89,8 @@ async def handle_chat(message):
 
 
 @dp.message_handler(commands=['model'])
-async def handle_model(message):
+async def handle_model(message: types.Message):
+    # Choose model from dict
     model = message.text.split()[1]
     models = {'davinci03': "text-davinci-003", 'davinci02': "text-davinci-002",
               'curie': "text-curie-001", 'babbage': "text-babbage-001", 'ada': "text-ada-001"}
@@ -90,9 +103,25 @@ async def handle_model(message):
 
 
 @dp.message_handler(commands=['help'])
-async def handle_help(message):
-    available_models = "davinci03, davinci02, curie, babbage, ada"
-    await message.reply("Welcome to the GPT-3 bot! Use the command /gpt followed by your prompt to generate a response. To use Gpt-3.5-turbo use command /chatgpt - BETA")
+async def handle_help(message: types.Message):
+    # Set help message
+    await message.reply("Use the command /gpt followed by your prompt to generate a response. To use Gpt-3.5-turbo use command /chatgpt - BETA")
     await message.reply(f"Use /model option followed by the desired model when running the script. Available models: {available_models}")
+
+
+@dp.message_handler(commands=['count_users'])
+async def count_users(message: types.Message):
+    # Get the chat ID from the message
+    chat_id = message.chat.id
+
+    # Get the chat members count
+    chat = await bot.get_chat(chat_id)
+    count = await chat.get_members_count()
+
+    # Send the count back to the user
+    await message.reply(f"There are {count} users in this chat.")
+
+executor.start_polling(dp)
+
 
 executor.start_polling(dp)
